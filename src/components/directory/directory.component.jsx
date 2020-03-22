db9b523f5ed64d03be6d7d510b491619fdaedb7d
AndriyPolukhin <andriy.polukhin@gmail.com>
2020-03-22 15:40:57 +0200

    broken our homepage into smaller components, finished styling our menu-item component

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import "./directory.styles.scss";
+
+import MenuItem from "../menu-item/menu-item.component";
+import sectionsData from "./directory.data";
+
+const Directory = () => {
+  const [sections] = useState(sectionsData);
+
+  return (
+    <div className="directory-menu">
+      {sections.map(({ title, imageUrl, id, size }) => (
+        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+      ))}
+    </div>
+  );
+};
+
+export default Directory;
