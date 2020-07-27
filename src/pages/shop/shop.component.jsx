4af16abb6df4fefbd849d5669c9c70f4c8609bd1
AndriyPolukhin <andriy.polukhin@gmail.com>
2020-07-27 21:00:46 +0300

    unusded code commented

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,35 +1,36 @@
 import React, { useEffect } from 'react';
 import { Route } from 'react-router-dom';
 
-import { useSelector, shallowEqual, useDispatch } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+// import { useSelector, shallowEqual, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
+// import { createStructuredSelector } from 'reselect';
+// import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
-import CollectionPage from '../collection/collection.component';
+// import CollectionPage from '../collection/collection.component';
 import CollectionPageContainer from '../collection/collection.container';
 
-import WithSpinner from '../../components/with-spinner/with-spinner.component';
+// import WithSpinner from '../../components/with-spinner/with-spinner.component';
 
 import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
-import {
-  selectIsCollectionFetching,
-  selectIsCollectionsLoaded,
-} from '../../redux/shop/shop.selectors';
+// import {
+//   selectIsCollectionFetching,
+//   selectIsCollectionsLoaded,
+// } from '../../redux/shop/shop.selectors';
 
 import './shop.styles.scss';
 
-const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
-const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+// const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+// const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 const ShopPage = ({ match }) => {
-  const state = useSelector((state) => state, shallowEqual);
-  const {
-    isCollectionFetching,
-    isCollectionsLoaded,
-  } = createStructuredSelector({
-    isCollectionFetching: selectIsCollectionFetching,
-    isCollectionsLoaded: selectIsCollectionsLoaded,
-  })(state);
+  // const state = useSelector((state) => state, shallowEqual);
+  // const {
+  //   isCollectionFetching,
+  //   isCollectionsLoaded,
+  // } = createStructuredSelector({
+  //   isCollectionFetching: selectIsCollectionFetching,
+  //   isCollectionsLoaded: selectIsCollectionsLoaded,
+  // })(state);
 
   const dispatch = useDispatch();
   useEffect(() => dispatch(fetchCollectionsStartAsync()), [dispatch]);
